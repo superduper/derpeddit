315956d269a7b7d9e267aac3aab257b9e29f827a
Victor Miroshnikov <me@vmiroshnikov.com>
2014-08-04 08:20:49 +0200

    I mean reaally remove csrf checks

--- project/settings.py
+++ project/settings.py
@@ -95,6 +95,7 @@ MIDDLEWARE_CLASSES = (
 #    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+	'core.middleware.DisableCSRF',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )

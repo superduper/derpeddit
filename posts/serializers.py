70fa2b861c0c7c510cd3aeae7d49ef3fa817876f
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2014-08-07 12:06:40 +0200

    Add id to response with created post

--- posts/serializers.py
+++ posts/serializers.py
@@ -10,7 +10,7 @@ class PostCreateSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Post
-        fields = ('title', 'text', 'link')
+        fields = ('title', 'text', 'link', 'id')
 
 
 class CommentSerializer(serializers.ModelSerializer):
